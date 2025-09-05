ce7be163cab035da6a40fea75a5b45218d0fffea
Taimoor Gill <taimoorgill311@gmail.com>
2025-09-05 23:48:58 +0500

    Clean Up

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,20 @@
-'use client';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-// import Footer from './components/footer/footer';
-import "./globals.css";
-// import Header from './components/header/header';
-import { useState } from 'react';
-import React from 'react';
-import { CartProvider } from "./context/cartcontext";
+// app/layout.js
+import LayoutWrapper from "./components/LayoutWrapper/LayoutWrapper";
+import Providers from "./providers";
+
+export const metadata = {
+  title: "My E-Commerce Store",
+  description: "Buy the best products online",
+};
 
 export default function RootLayout({ children }) {
-    const [user, setUser ] = useState(null); 
-
-    return (
-        <html lang="en">
-            <head><title>StepHub</title><link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"></link></head>
-            
-
-
-            <body>
-                <CartProvider>
-                    {/* <Header /> */}
-                    <ToastContainer /> {/* Ensure ToastContainer is within CartProvider */}
-                    <main>
-                        {React.cloneElement(children, { setUser  })}
-                    </main>
-                    {/* <Footer /> */}
-                </CartProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className="bg-gray-100 text-gray-900">
+        <Providers>
+          <LayoutWrapper>{children}</LayoutWrapper>
+        </Providers>
+      </body>
+    </html>
+  );
 }
